2e77902f4fd32147c75611f39e150ecf4a2fa09c
Happiness Mhlongo <mhlongohappiness880@gmail.com>
2026-09-18 14:26:42 +0200

    Added get by id function and added it in the routes, now i can get an author by id

--- src/routes/author.ts
+++ src/routes/author.ts
@@ -1,10 +1,19 @@
 import { Router, Request, Response } from 'express';
 import { body, param, validationResult } from "express-validator";
-import {getAllAuthors, createAuthor} from '../controllers/author'
+import {getAllAuthors, createAuthor, getAuthorById} from '../controllers/author'
 
 const router = Router();
 
 router.get("/", getAllAuthors);
+
+router.get("/:id", [param("id").isInt().withMessage("Id must be an integar")],(req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array() });
+    }
+    getAuthorById(req,res)
+})
 
 router.post("/",[
     body("name").notEmpty().withMessage("Name is required"),
